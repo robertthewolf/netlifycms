a0d70735076e60040a51cb566a8d5875eb19ff86
Robert Wolf <wolfrob35@gmail.com>
2018-01-15 23:54:06 +0100

    fixed crashing deploy

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,16 @@
 module.exports = {
+  siteMetadata: {
+  title: `JNJ Art Group`,
+},
   plugins: [
     `gatsby-plugin-netlify-cms`,
+    `gatsby-plugin-typography`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
   ],
 }
