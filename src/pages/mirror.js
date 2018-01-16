5e66d71a982b00dccc6417b6ab7f7875406a39b6
Robert Wolf <wolfrob35@gmail.com>
2018-01-17 00:07:28 +0100

    fixed fail

--- src/pages/mirror.js
+++ src/pages/mirror.js
@@ -3,40 +3,17 @@ import Link from "gatsby-link";
 import Helmet from 'react-helmet';
 import Script from 'react-load-script';
 
-export default ({ data }) => {
+export default () => {
 
   return (
     <div>
-      {data.allMarkdownRemark.edges.map(({ node }) => (
-        <div key={node.id}>
-          <Link to={node.fields.slug}>
+        <div>
+          <Link>
           <h3>
-            {node.frontmatter.title}
           </h3>
           <p></p>
           </Link>
         </div>
-      ))}
     </div>
   );
 };
-
-export const query = graphql`
-  query IndexQuery {
-    allMarkdownRemark {
-      totalCount
-      edges {
-        node {
-          id
-          frontmatter {
-            title
-          }
-          fields {
-            slug
-          }
-          excerpt
-        }
-      }
-    }
-  }
-`;
